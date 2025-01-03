9c9cc19ab0e404e6be29aeecf83fa449690ed285
unknown <abiram400@gmail.com>
2025-01-03 23:42:29 +0330

    refactor ui resume and insert js v1

---
+++ js/buttons.js
@@ -0,0 +1,21 @@
+document.getElementById('fa')?.addEventListener('click',e => {
+    location.replace('./resume-fa.html')
+})
+
+document.getElementById("en")?.addEventListener('click',e => {
+    location.replace('./index.html')
+})
+
+document.getElementById("de")?.addEventListener('click',e => {
+    location.replace('./resume-de.html')
+})
+
+document.getElementById("print").addEventListener('click',e => {
+    let container = document.getElementsByClassName('container')[0].innerHTML;
+    let body = document.body.innerHTML;
+
+    document.body.innerHTML = container;
+    window.print();
+
+    document.body.innerHTML = body;
+})
