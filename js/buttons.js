dac6b1ef3388c402b4f3315ff2bc3d52fca3f2f2
unknown <abiram400@gmail.com>
2025-01-04 01:51:02 +0330

    debug responsive and handler click print

--- js/buttons.js
+++ js/buttons.js
@@ -1,24 +1,7 @@
-document.getElementById('fa')?.addEventListener('click',e => {
-    location.replace('./resume-fa.html')
-})
+changeLanguage();
+document.getElementById("printResume").addEventListener('click',handlerPrint)
 
-document.getElementById("en")?.addEventListener('click',e => {
-    location.replace('./index.html')
-})
-
-document.getElementById("de")?.addEventListener('click',e => {
-    location.replace('./resume-de.html')
-})
-
-document.getElementById("print").addEventListener('click',e => {
-    let container = document.getElementsByClassName('container')[0].innerHTML;
-    let body = document.body.innerHTML;
-
-    document.body.innerHTML = container;
-    window.print();
-
-    document.body.innerHTML = body;
-
+function changeLanguage() {
     document.getElementById('fa')?.addEventListener('click',e => {
         location.replace('./resume-fa.html')
     })
@@ -29,5 +12,17 @@ document.getElementById("print").addEventListener('click',e => {
     
     document.getElementById("de")?.addEventListener('click',e => {
         location.replace('./resume-de.html')
-    })
-})
+    })    
+}
+
+function handlerPrint() {
+    let container = document.getElementsByClassName('container')[0].innerHTML;
+    let body = document.body.innerHTML;
+
+    document.body.innerHTML = container;
+    window.print();
+
+    document.body.innerHTML = body;
+    changeLanguage()
+    document.getElementById("printResume").addEventListener('click',handlerPrint)
+}
